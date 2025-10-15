e1548e2cb55950f2c32669eacf86dc9dea9c4e54
edwinsefer <edwinsefer@gmail.com>
2025-10-15 14:34:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,14 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 import datetime
-
 Firebase init
 if not firebase_admin._apps:
     cred = credentials.Certificate("serviceAccountKey.json")
     firebase_admin.initialize_app(cred)
 db = firestore.client()
-
 st.title("Edwin Family Tree App")
-
 menu = ["Add Member", "View Family Tree"]
 choice = st.sidebar.selectbox("Menu", menu)
-
 if choice == "Add Member":
     with st.form("entry_form"):
         name = st.text_input("Name")
@@ -22,5 +18,5 @@ if choice == "Add Member":
         phone = st.text_input("Mobile Number")
         address = st.text_area("Address")
         spouse = st.text_input("Spouse Name")
-
         children = st.text_area("Children (comma separated)")
+
